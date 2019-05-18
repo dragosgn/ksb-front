c82a7e9aa03c5adc96dd3b6e897fb377ed6cd1c4
Dragos Nedelcu <dragos.nedelcu@lokalleads.de>
2019-05-18 20:53:44 +0200

    fixes dsajdas

---
+++ src/Recongnized.js
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components";
+
+const Root = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+export default ({ currentView, setCurrentView }) => <Root>sdsadas</Root>;
