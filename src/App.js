bb17eee13bc51081216180fd610a330d88b9342e
Dragos Nedelcu <dragos.nedelcu@lokalleads.de>
2019-05-18 20:10:07 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React from "react";
-import "./App.css";
+import styled from "styled-components";
 
 import Scan from "./Scan";
+import Start from "./Start";
 
-function App() {
-  return (
-    <div className="App">
-      <Scan />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentView: "start"
+    };
+  }
+  nextView = view => {
+    this.setState({
+      currentView: view
+    });
+  };
+  render() {
+    return (
+      <Root>
+        {this.state.currentView === "start" && <Start />}
+        {this.state.currentView === "scan" && <Scan />}
+      </Root>
+    );
+  }
 }
 
 export default App;
+
+const Root = styled.div`
+  background-color: white;
+`;
+
+const Button = styled.div``;
---
+++ src/Start.js
@@ -0,0 +1,6 @@
+import React from "react";
+import styled from "styled-components";
+
+const Root = styled.div``;
+
+export default ({ nextView }) => <Root>Start</Root>;
