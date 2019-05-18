df5ced814e89cbf239266875ef3c1303e559e2bf
Dragos Nedelcu <dragos.nedelcu@lokalleads.de>
2019-05-18 19:45:12 +0200

    first commit

---
+++ src/Scan.js
@@ -0,0 +1,85 @@
+import React from "react";
+// Import COCO-SSD model as cocoSSD
+import * as cocoSSD from "@tensorflow-models/coco-ssd";
+
+export default class extends React.Component {
+  componentDidMount() {
+    this.webcam_init();
+    this.predictWithCocoModel();
+  }
+
+  webcam_init = () => {
+    this.video = document.getElementById("vid");
+
+    navigator.mediaDevices
+      .getUserMedia({
+        audio: false,
+        video: {
+          facingMode: "user"
+        }
+      })
+      .then(stream => {
+        this.video.srcObject = stream;
+        this.video.onloadedmetadata = () => {
+          this.video.play();
+        };
+      });
+  };
+
+  detectFrame = (video, model) => {
+    model.detect(video).then(predictions => {
+      this.renderPredictions(predictions);
+      requestAnimationFrame(() => {
+        this.detectFrame(video, model);
+      });
+    });
+  };
+
+  renderPredictions = predictions => {
+    const canvas = document.getElementById("canvas");
+
+    const ctx = canvas.getContext("2d");
+
+    canvas.width = 375;
+    canvas.height = 667;
+
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    // Font options.
+    const font = "16px sans-serif";
+    ctx.font = font;
+    ctx.textBaseline = "top";
+    ctx.drawImage(this.video, 0, 0, 375, 667);
+
+    predictions.forEach(prediction => {
+      const x = prediction.bbox[0];
+      const y = prediction.bbox[1];
+      const width = prediction.bbox[2];
+      const height = prediction.bbox[3];
+      // Draw the bounding box.
+      ctx.strokeStyle = "#00FFFF";
+      ctx.lineWidth = 2;
+      ctx.strokeRect(x, y, width, height);
+      // Draw the label background.
+      ctx.fillStyle = "#00FFFF";
+      const textWidth = ctx.measureText(prediction.class).width;
+      const textHeight = parseInt(font, 10); // base 10
+      ctx.fillRect(x, y, textWidth + 4, textHeight + 4);
+    });
+
+    predictions.forEach(prediction => {
+      const x = prediction.bbox[0];
+      const y = prediction.bbox[1];
+      // Draw the text last to ensure it's on top.
+      ctx.fillStyle = "#000000";
+      ctx.fillText(prediction.class, x, y);
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <p>This is the button row</p>
+      </div>
+    );
+  }
+}
